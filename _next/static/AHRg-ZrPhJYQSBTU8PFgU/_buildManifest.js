94c44d668f160617af2b067f4eaddee1e19abe15
gregrickaby <gregrickaby@users.noreply.github.com>
2022-04-05 15:18:26 +0000

    deploy: 300bc9c2293be69771bcfd932e3ad9f3b2ec3042

---
+++ _next/static/AHRg-ZrPhJYQSBTU8PFgU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/955229c28454e75a.css","static/chunks/pages/index-4f32528bf49cbc5f.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
